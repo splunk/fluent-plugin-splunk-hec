284643e2b005bbdd06fb5e03beef86a386f79f74
Shubham Jain <shubham@splunk.com>
2019-01-14 10:50:33 -0800

    build and push fluentd-hec-plugin image (#25)

--- .circleci/push.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-sudo apt-get install -y python-pip libpython-dev > /dev/null 2>&1
-sudo pip install awscli
-aws s3 cp pkg/fluent-plugin-splunk-hec-*.gem s3://circleci-k8s-fluentd-hec/
