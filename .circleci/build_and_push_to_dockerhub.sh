a0df866f8122f187b2543721a10a5581e7f87038
Shubham Jain <shujain@splunk.com>
2019-03-05 14:18:04 -0800

    release prep

--- .circleci/build_and_push_to_dockerhub.sh
+++ .circleci/build_and_push_to_dockerhub.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -e
 echo "Building docker image..."
-cp pkg/fluent-plugin-splunk-hec-*.gem docker
+cp /tmp/pkg/fluent-plugin-splunk-hec-*.gem docker
 VERSION=`cat VERSION`
 docker build --build-arg VERSION=$VERSION --no-cache -t splunk/fluent-plugin-splunk-hec:ci ./docker
 docker tag splunk/fluent-plugin-splunk-hec:ci $DOCKERHUB_ACCOUNT_ID/${DOCKERHUB_REPO_NAME}:${VERSION}
 echo "Push docker image to splunk dockerhub..."
 docker login --username=$DOCKERHUB_ACCOUNT_ID --password=$DOCKERHUB_ACCOUNT_PASS
 docker push splunk/${DOCKERHUB_REPO_NAME}:${VERSION} | awk 'END{print}'
-echo "Docker image pushed successfully."
+echo "Docker image pushed successfully to docker-hub."
--- .circleci/push_gem_to_rubygems.sh
+++ .circleci/push_gem_to_rubygems.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -e
-rake build -t -v
-echo "Pushing metrics gem to rubygems.org..."
+
+echo "Pushing fluent-plugin-splunk-hec gem to rubygems.org..."
 echo "gem `gem --version`"
 cat .circleci/gem_credentials | sed -e "s/__RUBYGEMS_API_KEY__/${RUBYGEMS_API_KEY}/" > ~/.gem/credentials
 chmod 0600 ~/.gem/credentials
-gem push pkg/fluent-plugin-splunk-hec-*.gem
+#Reenable to push to ruby gems
+#gem push /tmp/pkg/fluent-plugin-splunk-hec-*.gem
