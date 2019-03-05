58989edea08e2965af39717aa23527e0b3066174
Shubham Jain <shubham@splunk.com>
2019-03-05 13:42:51 -0800

    fixed docker login step (#44)

--- .circleci/build_and_push_to_dockerhub.sh
+++ .circleci/build_and_push_to_dockerhub.sh
@@ -7,5 +7,5 @@ docker build --build-arg VERSION=$VERSION --no-cache -t splunk/fluent-plugin-spl
 docker tag splunk/fluent-plugin-splunk-hec:ci $DOCKERHUB_ACCOUNT_ID/${DOCKERHUB_REPO_NAME}:${VERSION}
 echo "Push docker image to splunk dockerhub..."
 docker login --username=$DOCKERHUB_ACCOUNT_ID --password=$DOCKERHUB_ACCOUNT_PASS
-docker push $DOCKERHUB_ACCOUNT_ID/${DOCKERHUB_REPO_NAME}:${VERSION} | awk 'END{print}'
+docker push splunk/${DOCKERHUB_REPO_NAME}:${VERSION} | awk 'END{print}'
 echo "Docker image pushed successfully."
