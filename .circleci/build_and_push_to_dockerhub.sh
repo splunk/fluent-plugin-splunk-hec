7d48b5c74af22f32d5d38ba206f6b32fb06c21f3
Ryler Hockenbury <rhockenbury@predictivetechnologies.com>
2019-07-23 15:13:17 -0400

    Add windows server dockerfile

--- .circleci/build_and_push_to_dockerhub.sh
+++ .circleci/build_and_push_to_dockerhub.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e
-echo "Building docker image..."
-cp /tmp/pkg/fluent-plugin-splunk-hec-*.gem docker
+echo "Building linux docker image..."
+cp /tmp/pkg/fluent-plugin-splunk-hec-*.gem docker/linux
 VERSION=`cat VERSION`
-docker build --build-arg VERSION=$VERSION --no-cache -t splunk/fluent-plugin-splunk-hec:ci ./docker
+docker build --build-arg VERSION=$VERSION --no-cache -t splunk/fluent-plugin-splunk-hec:ci ./docker/linux
 docker tag splunk/fluent-plugin-splunk-hec:ci splunk/${DOCKERHUB_REPO_NAME}:${VERSION}
 echo "Push docker image to splunk dockerhub..."
 docker login --username=$DOCKERHUB_ACCOUNT_ID --password=$DOCKERHUB_ACCOUNT_PASS
