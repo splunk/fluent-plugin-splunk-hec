23a52743e5094394a99bb393f4e95148c696713d
Rock Baek <rbaek@splunk.com>
2019-11-15 10:56:17 -0800

    fluentd version fix to 1.7.0 and trigger func test

--- .circleci/build_and_push_to_dockerhub.sh
+++ .circleci/build_and_push_to_dockerhub.sh
@@ -5,7 +5,9 @@ cp /tmp/pkg/fluent-plugin-splunk-hec-*.gem docker
 VERSION=`cat VERSION`
 docker build --no-cache -t splunk/fluent-plugin-splunk-hec:ci ./docker
 docker tag splunk/fluent-plugin-splunk-hec:ci splunk/${DOCKERHUB_REPO_NAME}:${VERSION}
+docker tag splunk/fluent-plugin-splunk-hec:ci splunk/${DOCKERHUB_REPO_NAME}:latest
 echo "Push docker image to splunk dockerhub..."
 docker login --username=$DOCKERHUB_ACCOUNT_ID --password=$DOCKERHUB_ACCOUNT_PASS
 docker push splunk/${DOCKERHUB_REPO_NAME}:${VERSION} | awk 'END{print}'
+docker push splunk/${DOCKERHUB_REPO_NAME}:latest | awk 'END{print}'
 echo "Docker image pushed successfully to docker-hub."
