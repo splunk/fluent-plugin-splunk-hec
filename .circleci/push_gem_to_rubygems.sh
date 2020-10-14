4bb5f70f3888da9cc355112621926e2e72db50c4
Rock Baek <rbaek@splunk.com>
2020-10-13 18:05:12 -0700

    change it to main (#149)

--- .circleci/push_gem_to_rubygems.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-echo "Pushing HEC gem to rubygems.org..."
-echo "gem `gem --version`"
-cat .circleci/gem_credentials | sed -e "s/__RUBYGEMS_API_KEY__/${RUBYGEMS_API_KEY}/" > ~/.gem/credentials
-chmod 0600 ~/.gem/credentials
-gem push /tmp/pkg/fluent-plugin-splunk-hec-*.gem
